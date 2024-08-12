088519c6aa07f88f2567a0afefcc9f98c1489588
Zeinot <60817247+Zeinot@users.noreply.github.com>
2024-08-12 14:25:26 +0100

    s

--- src/app/[locale]/(dashboard)/dashboard/create/CreatePostForm.tsx
+++ src/app/[locale]/(dashboard)/dashboard/create/CreatePostForm.tsx
@@ -159,8 +159,7 @@ export default function CreatePostForm() {
               {...register("date")}
               type="datetime-local"
               id="date"
-              name="date"
-              value="2018-06-12T19:30"
+              name="date" s
               className="px-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
             />
             <p className="text-red-600"> {errors.date?.message}</p>
