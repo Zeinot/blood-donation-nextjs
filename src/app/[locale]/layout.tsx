e83ee9e11bef252e2bc2e0c8ecddfed4bb8fc95f
Zeinot <60817247+Zeinot@users.noreply.github.com>
2024-08-12 11:14:33 +0100

    ss

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,35 +1,24 @@
-import { GlobalContextProvider } from '@/context/store';
-import {NextIntlClientProvider} from 'next-intl';
-import {getMessages} from 'next-intl/server';
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css"; 
-
-const inter = Inter({ subsets: ["latin"] });
-
-export const metadata: Metadata = {
-  title: "Blood Donation",
-  description: "Blood Donation",
-};
+import { NextIntlClientProvider } from "next-intl";
+import { getMessages } from "next-intl/server"; 
+ 
+ 
 
 export default async function LocaleLayout({
   children,
-  params: {locale}
+  params: { locale },
 }: {
   children: React.ReactNode;
-  params: {locale: string};
+  params: { locale: string };
 }) {
   // Providing all messages to the client
   // side is the easiest way to get started
   const messages = await getMessages();
- 
+
   return (
     <html lang={locale}>
       <body>
         <NextIntlClientProvider messages={messages}>
-        <GlobalContextProvider>
           {children}
-          </GlobalContextProvider>
         </NextIntlClientProvider>
       </body>
     </html>
--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,6 +1,19 @@
 import {useTranslations} from 'next-intl';
+import Footer from '../_HomePageComponents/Footer';
+import OurMission from '../_HomePageComponents/OurMission';
+import Stats from '../_HomePageComponents/Stats';
+
+import Logos from '../_HomePageComponents/Logos';
+import Testamonial from '../_HomePageComponents/Testamonial';
+import Hero from '../_HomePageComponents/Hero';
  
 export default function HomePage() {
   const t = useTranslations('HomePage');
-  return <h1>{t('title')}</h1>;
+  return <h1>{t('title')}
+      <Hero />
+      <Logos />
+      <Testamonial />
+      <Stats />
+      <OurMission />
+      <Footer /></h1>;
 }
