10d38c557f797adf5ff8580bc54c267e4c39d9e0
Zeinot <60817247+Zeinot@users.noreply.github.com>
2024-08-12 14:07:32 +0100

    S

--- src/app/[locale]/(dashboard)/dashboard/create/CreatePostForm.tsx
+++ src/app/[locale]/(dashboard)/dashboard/create/CreatePostForm.tsx
@@ -81,7 +81,7 @@ export default function CreatePostForm() {
               id="city"
               name="city"
               type="text"
-              placeholder="City ..."
+              placeholder={t("City")}
               className="px-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
             />
             <p className="text-red-600"> {errors.city?.message}</p>
@@ -92,7 +92,7 @@ export default function CreatePostForm() {
             htmlFor="location"
             className="block text-sm font-medium leading-6 text-gray-900"
           >
-           { t("Location")}
+            {t("Location")}
           </label>
           <div className="mt-2">
             <input
@@ -100,7 +100,7 @@ export default function CreatePostForm() {
               id="location"
               name="location"
               type="text"
-              placeholder="Location ..."
+              placeholder={t("Location")}
               className="px-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
             />
             <p className="text-red-600"> {errors.location?.message}</p>
--- src/app/[locale]/(dashboard)/dashboard/edit/[postId]/UpdatePostForm.tsx
+++ src/app/[locale]/(dashboard)/dashboard/edit/[postId]/UpdatePostForm.tsx
@@ -112,7 +112,7 @@ export default function UpdatePostForm({ post }: { post: Post }) {
               id="city"
               name="city"
               type="text"
-              placeholder="City ..."
+              placeholder={t("City")}
               className="px-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
             />
             <p className="text-red-600"> {errors.city?.message}</p>
